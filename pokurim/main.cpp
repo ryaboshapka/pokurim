c219a582b75744588d52c175fdc7003db78ce0d7
ryaboshapka <remagnetization@gmail.com>
2014-12-30 21:22:02 +0300

    noth

---
+++ pokurim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pokurim
+//
+//  Created by abricos on 30.12.14.
+//  Copyright (c) 2014 abricos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
